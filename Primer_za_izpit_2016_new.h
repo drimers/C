9bde9d06376e32daf42b78d4d45493bca95ba8a9
drimers <stefan.p.petkov@gmail.com>
2017-02-03 16:39:17 +0200

    Add files via upload

---
+++ Primer_za_izpit_2016_new.h
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+/*Опитайте да решите следната задача (а утре ще пратя примерно решение и още задачи):
+Да се дефинират функции за:
+- извеждане на числов масив, като всеки елемент е сам на ред и във формат <индекс>--::--<стойност>;
+- намиране на индекса на първи елемент в числов масив, отличаващ се от дадено число с най-много единица.
+С тези функции да се напише програма, която въвежда числов масив от 7 елемента и число R и извежда масива и
+индекса на първия елемент във въведения масив, отличаващ се от R с най-много единица.
+*/
+
+const int n = 7;
+//int arr[n] = { 2,5,6,7,7,2,2,2,9 };
+int arr[n];
+int i , R;
+
+void write(int arrey[n]);
+void print(int arrey[n]);
+void find(int r, int arrey[n]);
+
+int main() {
+	write(arr);
+	print(arr);
+
+	cout << "Vavedete R " << endl;
+	cin >> R;
+
+	find(R, arr);
+	system("pause");
+	return 0;
+}
+
+void write(int arrey[n]) {
+	cout << "Vavedete sedem chisla :" << endl;
+	for (i = 0; i < n; i++) {
+		cin >> arrey[i];
+	}
+}
+
+void print(int arrey[n]) {
+	for (i = 0; i < n; i++) {
+		cout << "index [" << i << "] :: " << arr[i] << endl;
+	}
+}
+
+void find(int r, int arrey[n]) {
+	for (i = 0; i < n; i++) {
+		if (arrey[i] == R || arrey[i] - 1 == R || arrey[i] + 1 == R) {
+			cout << "First index  : " << i << endl; break;
+		}
+	} 
+}
